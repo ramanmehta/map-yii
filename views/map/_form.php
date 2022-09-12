190a74e0c23775d7c6ead4ba0c5558af49d1790c
Software Miles <smtekki@pop-os.localdomain>
2022-09-12 22:51:46 +0530

    get latitude and longitude save to db

--- views/map/_form.php
+++ views/map/_form.php
@@ -14,59 +14,60 @@ use yii\widgets\ActiveForm;
 <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 <style>
 .modal-content{
-height: 600px;
-width: 100%;
+height: 768px;
+width: 1024px;
 }
 </style>
 
-<div class="map-poi-form">
 
-    <?php $form = ActiveForm::begin(); ?>
-
-
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-    <br>
-    <div class="row">
-        <div class="col">
-            <?= $form->field($model, 'latitude')->textInput(['maxlength' => true]) ?>
-        </div>
-        <div class="col">
-            <?= $form->field($model, 'longitude')->textInput(['maxlength' => true]) ?>
-        </div>
-        <div class="col">
-            <br>
-            <input type="button" value="Get Location" id="location" class="btn btn-success" data-toggle="modal" data-target="#mapModalCenter">
-            <!-- <?= Html::button('Get Location', ['class' => 'btn btn-success']) ?> -->
-        </div>
-    </div>
-    <br>
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
-</div>
 
 <!-- Button trigger modal -->
-
+<div class="row">
+   
+    <div class="col">
+        <br>
+        <input type="button" value="Get Location" id="location" class="btn btn-success" data-toggle="modal" data-target="#mapModalCenter">
+        <!-- <?= Html::button('Get Location', ['class' => 'btn btn-success']) ?> -->
+    </div>
+</div>
 
 <!-- Modal -->
 <div class="modal fade" id="mapModalCenter" tabindex="-1" role="dialog" aria-labelledby="mapModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content" height="600">
+    <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">Choose Location</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div id="map" class="modal-body" height='600'>
-
+      <div id="map" class="modal-body">
       </div>
       <div class="modal-footer">
+            <div class="map-poi-form">
+
+        <?php $form = ActiveForm::begin(); ?>
+
+
+        <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+        <br>
+        <div class="row">
+            <div class="col">
+                <?= $form->field($model, 'latitude')->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="col">
+                <?= $form->field($model, 'longitude')->textInput(['maxlength' => true]) ?>
+            </div>
+        </div>
+        <br>
+        
+
+        </div>
+        <div class="form-group">
+            <?= Html::submitButton('Save changes', ['class' => 'btn btn-success']) ?>
+        </div>
+        <?php ActiveForm::end(); ?>
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
@@ -77,7 +78,7 @@ width: 100%;
       defer
     ></script>
 
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <!-- <script type="text/javascript">
     var lati = document.getElementsByName('latitude');
     var longi = document.getElementsByName('longitude');
@@ -94,36 +95,92 @@ width: 100%;
     }
 </script> -->
 <script type="text/javascript">
+
 /**
  * @license
  * Copyright 2019 Google LLC. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+
+var defLat = '20.5937';
+var defLng = '78.9629';
+$.getJSON("https://api.ipify.org/?format=json", function(e) {
+    var defip = e.ip;
+    if(defip){
+        $.getJSON('http://ip-api.com/json/'+defip+'?fields=status,message,lat,lon,query')  
+        .done (function(location)
+        {
+            defLat = location.lat;
+            defLng = location.lon;
+            // console.log(location.lat);
+            // alert(defLat);
+            // $('#latitude').html(location.latitude);
+            // $('#longitude').html(location.longitude);
+        });
+    }
+    // console.log(e);
+});
 function initMap() {
-  const myLatlng = { lat: -25.363, lng: 131.044 };
+    
+  const myLatlng = { lat: parseFloat(defLat), lng: parseFloat(defLng) };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 6,
     center: myLatlng,
   });
   // Create the initial InfoWindow.
+
+  let marker = new google.maps.Marker({
+                    position: myLatlng,
+                    title:"Hello World!"
+                });
+    // marker.setMap(map);             
   let infoWindow = new google.maps.InfoWindow({
     content: "Click the map to get Lat/Lng!",
     position: myLatlng,
   });
 
   infoWindow.open(map);
+
   // Configure the click listener.
+//   var listener1 = marker.addListener('click', aFunction);
+
+
   map.addListener("click", (mapsMouseEvent) => {
     // Close the current InfoWindow.
-    infoWindow.close();
+    $("#mappoi-latitude").val('');
+    $("#mappoi-longitude").val('');
+    marker.setMap(null);
+
+    marker = new google.maps.Marker({
+                    position: mapsMouseEvent.latLng,
+                    title:JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2),
+                    //icon: icons[features[i].type].icon
+                });
+
+    var myLatLng = mapsMouseEvent.latLng;
+    var lat = myLatLng.lat();
+    var lng = myLatLng.lng();
+                
+    // let latlng = JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2);
+    // console.log(JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2));
+    // alert(lat);
+    // alert(latlng.lng)
+    $("#mappoi-latitude").val(lat);
+    $("#mappoi-longitude").val(lng);
+    // infoWindow.close();
+    marker.setMap(map); 
     // Create a new InfoWindow.
-    infoWindow = new google.maps.InfoWindow({
-      position: mapsMouseEvent.latLng,
-    });
-    infoWindow.setContent(
-      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-    );
-    infoWindow.open(map);
+    // infoWindow = new google.maps.InfoWindow({
+    //   position: mapsMouseEvent.latLng,
+    // });
+    // infoWindow.setContent(
+    //   JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+    // );
+    // infoWindow.open(map);
+    
+
+
+
   });
 }
 
